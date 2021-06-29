bb7ec918b52dee8ecabaebcc3061635b56b83170
eohyemin <eohyemin@gmail.com>
2021-06-29 22:49:12 +0900

    lambda expression

--- src/main/java/com/example/java8to11/lambda/Foo.java
+++ src/main/java/com/example/java8to11/lambda/Foo.java
@@ -1,20 +1,50 @@
 package com.example.java8to11.lambda;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Foo {
     public static void main(String[] args) {
-        RunSomething runSomething = new RunSomething() {
-            @Override
-            public void doIt() {
-                System.out.println("RUN");
-            }
-        };
         RunSomething runSomething1 = () -> System.out.println("RUN");
 
+        // 입력 받은 값이 동일한 경우, 결과가 같아야 함
         ReturnSomething returnSomething = (num) -> num + 10;
         System.out.println(returnSomething.doIt(10));
         System.out.println(returnSomething.doIt(10));
 
-        int baseNumber = 100;
-        ReturnSomething returnSomething1 = num -> num + baseNumber;
+        Supplier<Integer> get10 = () -> 10;
+        BinaryOperator<Integer> sum = (a, b) -> a + b;
+        // 컴파일러 타입 추론
+
+        Foo foo = new Foo();
+        foo.run();
+    }
+
+    private void run() {
+        // 변수 캡쳐
+        // effective final인 변수만 참조 가능
+        final int baseNumber = 10;
+
+        // 로컬 클래스
+        class LocalClass {
+            void printBaseNumber() {
+                System.out.println(baseNumber);
+            }
+        }
+
+        // 익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(baseNumber); // 쉐도잉
+            }
+        };
+
+        // 람다
+        Consumer<Integer> printInt = i -> System.out.println(i+baseNumber); // baseNumber와 같은 scope
+        printInt.accept(10);
+
     }
 }
